8c7070829aca24cccba5bbc9e1f06e01d777cefa
Kas Moors <kas.moors@student.kuleuven.be>
2025-03-18 12:11:46 +0100

    aanpassingen mergesort

--- 3_merge_sort/main.py
+++ 3_merge_sort/main.py
@@ -44,11 +44,27 @@ for n in range(0, step_count*step_size, step_size):
     random.shuffle(array)
     counts.append(mergeSort(array))
 
+counts_BC = []
+for n in range(0, step_count*step_size, step_size):
+    array = [i for i in range(n)]
+    counts_BC.append(mergeSort(array))
+
+counts_equals = []
+for n in range(0, step_count*step_size, step_size):
+    array = [4 for i in range(n)]
+    counts_equals.append(mergeSort(array))
+
+
 import matplotlib.pyplot as plt
 # plot experimental data
-plt.scatter(range(0, step_count*step_size, step_size), counts, label='Experimentele data', color='blue')
+plt.scatter(range(0, step_count*step_size, step_size), counts, label='Experimentele data average case', color='blue')
+plt.scatter(range(0, step_count*step_size, step_size), counts_BC, label='Experimentele data best case', color='green')
 # plot best case
 plt.plot(range(0, step_count*step_size, step_size), [x*math.log2(x) for x in range(1, step_count*step_size, step_size)], color='red', label='Worst case: n*log(n)')
 # plot best case
 plt.plot(range(0, step_count*step_size, step_size), [(x/2)*math.log2(x) for x in range(1, step_count*step_size, step_size)], color='green', label='Best case: (n/2)log(n)')
+plt.xlabel("Lengte lijst")
+plt.ylabel("Aantal vergelijkingen")
+plt.legend()
+plt.title("complexiteit merge sort")
 plt.show()
