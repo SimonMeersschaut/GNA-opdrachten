6131e4f8384708aec7f67472350dfc57ad06ce8b
Kas Moors <kas.moors@student.kuleuven.be>
2025-03-19 17:41:28 +0100

    verslag quicksort

--- 4_quick_sort/main.py
+++ 4_quick_sort/main.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 
@@ -8,18 +9,22 @@ def hoare_partition(arr, low, high, count):
 
     while True:
         i += 1
+        if arr[i] >= pivot:
+            count += 1
         while arr[i] < pivot:
             count += 1
             i += 1
 
         j -= 1
+        if arr[j] <= pivot:
+            count += 1
         while arr[j] > pivot:
             count += 1
             j -= 1
 
         if i >= j:
-            count += 1
             return j , count
+
 
         arr[i], arr[j] = arr[j], arr[i]
 
@@ -56,16 +61,14 @@ def quickSortLomuto(arr, low, high, count = 0):
 
 
 counts = []
-step_size = 500
-step_count = 100
+step_size = 50
+step_count = 200
 for n in range(0, step_count*step_size, step_size):
     array = [i for i in range(n)]
     random.shuffle(array)
     counts.append(quickSortHoare(array, 0, len(array) - 1))
 
 counts_l = []
-step_size = 500
-step_count = 100
 for n in range(0, step_count*step_size, step_size):
     array = [i for i in range(n)]
     random.shuffle(array)
@@ -73,7 +76,14 @@ for n in range(0, step_count*step_size, step_size):
 
 import matplotlib.pyplot as plt
 # plot experimental data
-plt.scatter(range(0, step_count*step_size, step_size), counts, label='Experimentele data average case', color='blue')
-plt.scatter(range(0, step_count*step_size, step_size), counts_l, label='Experimentele data average case', color='yellow')
-
+plt.scatter(range(0, step_count*step_size, step_size), counts, label='Experimentele data Hoare', color='blue')
+plt.scatter(range(0, step_count*step_size, step_size), counts_l, label='Experimentele data Lomuto', color='yellow')
+#plt.plot(range(0, step_count*step_size, step_size), [(x**2)/2 for x in range(1, step_count*step_size, step_size)], color='red', label='Best case: (n/2)log(n)')
+plt.plot(range(0, step_count*step_size, step_size), [1.39*x*math.log2(x) for x in range(1, step_count*step_size, step_size)], color='blue', label='Best case: 1,39*nlog(n)')
+plt.plot(range(0, step_count*step_size, step_size), [x*math.log2(x) for x in range(1, step_count*step_size, step_size)], color='green', label='Best case: nlog(n)')
+plt.xlabel("Lengte lijst")
+plt.ylabel("Aantal vergelijkingen")
+plt.legend()
+plt.title("complexiteit quicksort")
+plt.loglog()
 plt.show()
