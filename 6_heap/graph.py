19dc46b08e3cfac787b4658d918219b20245fe47
Simon Meersschaut <simon.meersschaut@gmail.com>
2025-04-23 15:12:00 +0200

    add heap-verslag

---
+++ 6_heap/graph.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from heap import Heap
+import random
+import math
+
+N = 1000 # 1.000
+
+comparisons_list = []
+h = Heap()
+
+for i in range(N):
+    h.add(random.randint(0, N))
+    comparisons_list.append(h.grab_count())
+
+# # plot experimental data
+plt.scatter(list(range(N)), comparisons_list, label='Comparisons Heap', color='blue')
+
+plt.plot(list(range(1, N)), [1+int(math.log2(x)) for x in range(1, N)], label='Comparisons Heap', color='red')
+# #
+plt.show()
+
+
+##########
+# delete maximum
+##########
+
+comparisons_list = []
+
+for i in range(N):
+    h.delete_max()
+    comparisons_list.append(h.grab_count())
+
+plt.scatter(list(range(N)), comparisons_list, label='Comparisons Heap (delete max)', color='blue')
+
+plt.plot(list(range(1, N)), [2*int(math.log2(N - x)) for x in range(1, N)], label='Comparisons Heap', color='red')
+#
+plt.show()
--- 6_heap/heap.py
+++ 6_heap/heap.py
@@ -1,15 +1,25 @@
 class Heap:
     def __init__(self):
         self.content = [None]
+        self.count = 0
     
     def add(self, value):
         self.content.append(value)
         self.swim(len(self.content) - 1)
 
     def swim(self, index: int):
-        while (index > 1 and self.content[index//2] < self.content[index]):
+        while (index > 1 and self.less(index//2, index)):
             self.content[index//2], self.content[index] = self.content[index], self.content[index//2]
             index = index//2
+    
+    def less(self, index1, index2):
+        self.count += 1
+        return self.content[index1] < self.content[index2]
+
+    def grab_count(self):
+        value = self.count
+        self.count = 0
+        return value
     
     def delete_max(self):
         # swap root with last element
@@ -25,10 +35,10 @@ class Heap:
         while 2*index <= len(self.content) - 1:
             kind_index = 2*index
             if (kind_index < len(self.content) - 1):
-                rechterkind_is_groter = (self.content[kind_index] < self.content[kind_index+1])
+                rechterkind_is_groter = self.less(kind_index, kind_index+1)
                 if rechterkind_is_groter:
                     kind_index += 1
-            if not self.content[kind_index] > self.content[index]:
+            if not self.less(index, kind_index):
                 break
             # exchange
             self.content[kind_index], self.content[index] = self.content[index], self.content[kind_index]
